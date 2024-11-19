0e0386a096f2cacd449d1f2e963d4af8008cb208
Naomi Johnson <snjohnson789@gmail.com>
2024-11-19 11:04:52 -0800

    accessing secret API keys

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
+import keys
 
 app = Flask(__name__)
 @app.route("/")
 def index():
+
     fruits = [
         {"name": "apples", "quantity": 3},
             {"name": "oranges", "quantity": 2},
             {"name": "strawberries", "quantity": 6}
     ]
-    return render_template("index.html", fruits=fruits)
+    return render_template("index.html", fruits=fruits,  key_1=keys.MY_SECRET_API_KEY_1,  key_2=keys.MY_SECRET_API_KEY_2)
